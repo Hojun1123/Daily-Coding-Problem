ad420a59584e1402e6fb466c9d05518015b90393
Hojun1123 <65999992+Hojun1123@users.noreply.github.com>
2023-11-29 21:24:34 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,25 +1,21 @@
 #include <iostream>
-#include <map>;
+#include <map>
 using namespace std;
 int main() {
 	int X;
 	cin >> X;
 	int table[1000000];
-	for (int i = 1; i < X; ++i) { table[i] = X; }
+	for (int i = 1; i < X; ++i)
+		table[i] = X;
+
 	table[X] = 0;
-	int* nt;
-	int* dt;
 	for (int i = X; i >= 1; --i)
 	{
-		nt = &table[i];
-		dt = &table[i / 3];
 		if (i % 3 == 0)
-			*dt = min(*nt + 1, *dt);
-		dt = &table[i / 2];
+			table[i / 3] = min(table[i] + 1, table[i / 3]);
 		if (i % 2 == 0)
-			*dt = min(*nt + 1, *dt);
-		dt = &table[i - 1];
-		*dt = min(*nt + 1, *dt);
+			table[i / 2] = min(table[i] + 1, table[i / 2]);
+		table[i - 1] = min(table[i] + 1, table[i - 1]);
 	}
 	cout << table[1];
 	return 0;
