3404455ac7ed832cdc75878f1b3eb6d9c23fd766
Hojun1123 <65999992+Hojun1123@users.noreply.github.com>
2023-12-24 17:35:10 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <algorithm>
+#include <list>
+using namespace std;
+int N;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int s = 1234567891;
+	char a;
+	int h;
+	cin >> N;
+	long long sum = 0;
+	long long p = 1;
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> a;
+		h = a - 'a' + 1;
+		p = 1;
+		for (int j = 0; j < i; ++j)
+		{
+			p *= 31;
+			p %= 1234567891;
+		}
+		sum += (h * p) % 1234567891;
+	}
+
+	cout << sum;
+	return 0;
+}
