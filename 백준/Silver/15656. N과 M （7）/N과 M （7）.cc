d8ea4aefe07855ba73abc53f131ebc5069ad4ce3
Hojun1123 <65999992+Hojun1123@users.noreply.github.com>
2024-01-24 22:13:12 +0900

    [Silver III] Title: N과 M (7), Time: 596 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <map>
+using namespace std;
+int M, N;
+int arr[8];
+int input[8];
+int check[8];
+void recursion(int depth)
+{
+	if (depth == M)
+	{
+		for (int i = 0; i < M; ++i)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+	for (int i = 0; i < N; ++i)
+	{
+		arr[depth] = input[i];
+		recursion(depth + 1);
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	cin >> N >> M;
+	for (int i = 0; i < N; ++i)
+		cin >> input[i];
+	sort(input, input + N);
+	recursion(0);
+	return 0;
+}
