663830b26fb63d709d8ff30a816bab0932d177a4
Hojun1123 <65999992+Hojun1123@users.noreply.github.com>
2024-02-12 12:10:27 +0900

    [Gold IV] Title: 인구 이동, Time: 484 ms, Memory: 392864 KB -BaekjoonHub

---
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -0,0 +1,121 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <string>
+#include <algorithm>
+using namespace std;
+int N, L, R;
+int arr[100][100];
+int check[100][100];
+int graph[100][100][100][100];
+int dy[4] = { 1, -1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+struct yx {
+    int y, x;
+};
+queue<yx> q;
+int populationSum, countryCnt;
+vector<yx> tmp;
+bool Open()
+{
+    bool openFlag = false;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            for (int k = 0; k < 4; ++k)
+            {
+                int y = dy[k] + i;
+                int x = dx[k] + j;
+                if (x >= 0 && y >= 0 && x < N && y < N)
+                {
+                    int diff = abs(arr[i][j] - arr[y][x]);
+                    if (diff >= L && diff <= R)
+                    {
+                        graph[y][x][i][j] = 1;
+                        openFlag = true;
+                    }
+                    else
+                    {
+                        graph[y][x][i][j] = 0;
+                    }
+                }
+            }
+        }
+    }
+    return openFlag;
+}
+
+void bfs()
+{
+    for (int i = 0; i < q.size(); ++i)
+    {
+        yx now = q.front();
+        q.pop();
+        tmp.push_back(now);
+        populationSum += arr[now.y][now.x];
+        for (int k = 0; k < 4; ++k)
+        {
+            int y = dy[k] + now.y;
+            int x = dx[k] + now.x;
+            if (x >= 0 && y >= 0 && x < N && y < N && check[y][x] == 0 && graph[now.y][now.x][y][x]) {
+                check[y][x] = 1;
+                q.push({ y, x });
+            }
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> N >> L >> R;
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < N; ++j)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int answer = 0;
+    while (true)
+    {
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+            {
+                check[i][j] = 0;
+            }
+        }
+        if (!Open())
+            break;
+        for (int i = 0; i < N; ++i)
+        {
+            for (int j = 0; j < N; ++j)
+            {
+                if (check[i][j] == 0)
+                {
+                    tmp = vector<yx>();
+                    q.push({ i, j });
+                    check[i][j] = 1;
+                    populationSum = 0;
+                    while (!q.empty())
+                    {
+                        bfs();
+                    }
+                    for (yx n : tmp)
+                    {
+                        arr[n.y][n.x] = populationSum / tmp.size();
+                    }
+                }
+            }
+        }
+        answer++;
+    }
+    cout << answer;
+    return 0;
+}
