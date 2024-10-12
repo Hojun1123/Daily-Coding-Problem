7de3f60a78b46e807702ed68221690d881a7ac68
Hojun1123 <65999992+Hojun1123@users.noreply.github.com>
2024-10-12 19:53:09 +0900

    [Gold V] Title: 도로의 개수, Time: 0 ms, Memory: 2104 KB -BaekjoonHub

--- 백준/Gold/1577. 도로의 개수/도로의 개수.cc
+++ 백준/Gold/1577. 도로의 개수/도로의 개수.cc
@@ -6,6 +6,7 @@
 #include <stack>
 #include <cmath>
 #include <map>
+#include <set>
 using namespace std;
 int N, M, K;
 long long dp[102][102];
@@ -22,7 +23,7 @@ struct Node {
         return false;
     }
 };
-map<Node, bool> um;
+set<Node> um;
 int main(int argc, char** argv)
 {
     ios::sync_with_stdio(0);
@@ -32,15 +33,15 @@ int main(int argc, char** argv)
     for (int i = 0; i < K; ++i) {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-        um[{a + 1, b + 1, c + 1, d + 1}] = true;
-        um[{c + 1, d + 1, a + 1, b + 1}] = true;
+        um.insert({a + 1, b + 1, c + 1, d + 1});
+        um.insert({ c + 1, d + 1, a + 1, b + 1 });
     }
     dp[1][1] = 1;
     for (int i = 1; i <= N + 1; ++i) {
         for (int j = 1; j <= M + 1; ++j) {
             //i, j는 자신보다 작은 위아래 칸의 합
-            if(um[{i, j, i - 1, j}] == false) dp[i][j] += dp[i - 1][j];
-            if(um[{i, j, i, j - 1}] == false) dp[i][j] += dp[i][j - 1];
+            if(um.find({i - 1, j, i, j}) == um.end()) dp[i][j] += dp[i - 1][j];
+            if(um.find({i, j -1, i, j}) == um.end()) dp[i][j] += dp[i][j - 1];
         }
     }
 
